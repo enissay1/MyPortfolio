9722679f991fc048487caa69952956232ec6b31d
enissay1 <yassine.ezzahr@gmail.com>
2022-08-13 22:21:52 +0100

    smooth nav

--- bannier.js
+++ bannier.js
@@ -2,9 +2,11 @@ let togg1 = document.getElementById("checkbox_toggle");
 let menu = document.getElementById("menu");
 
 togg1.addEventListener("click", () => {
-if(getComputedStyle(menu).display != "none"){
-menu.style.display = "none";
+if(getComputedStyle(menu).transform != "matrix(1, 0, 0, 0, 0, 0)"){
+    menu.style.transform = "scale(1,0)";
 } else {
-menu.style.display = "inline-block";
-}
-})
+//menu.style.display = "none";
+menu.style.transform = "scale(1,1)";
+
+
+}})
